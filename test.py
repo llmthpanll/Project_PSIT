1594ee6e75a05442c5f9a2d811ffceb664cd128b
Methasit <methasitpan@gmail.com>
2021-10-26 23:17:41 +0700

    Button

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ import tkinter as tk
 root = tk.Tk()
 root.option_add("*Font", "consolas 20")
 root.title("I love tkinter very much")
+label = tk.Label(text='Hello Pan').grid()
+b1 = tk.Button(text="Click me!")
 for i in range(10):
     tk.Label(root, text="hello world").grid() # Label -> widget
 root.mainloop()
---
+++ test2.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+app = tk.Tk()
+app.title('Hello world')
+button = tk.Button(text='click me!')
+button.pack()
+tk.Button(text='Looo').pack()
+app.mainloop()
