26b48e398cb83fb1f9261864e30d82a388c3befd
Methasit <methasitpan@gmail.com>
2021-10-28 14:28:53 +0700

    test 3rd

---
+++ function.py
@@ -0,0 +1,2 @@
+    def print_something():
+        print('Button bind working!')
--- test2.py
+++ test2.py
@@ -1,7 +1,11 @@
 import tkinter as tk
+def print_something(txt):
+        print(txt)
+        tk.Label(text='confuse').pack()
 app = tk.Tk()
 app.title('Hello world')
-button = tk.Button(text='click me!')
-button.pack()
+e1 = tk.Entry()
+e1.pack()
+button = tk.Button(text='click me!', command=print_something(e1)).pack()
 tk.Button(text='Looo').pack()
 app.mainloop()
