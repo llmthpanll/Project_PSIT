70b3a18acd8154b985dab9fed0d1823bac8d5d40
Methasit <methasitpan@gmail.com>
2021-11-07 20:02:00 +0700

    .

--- test2.py
+++ test2.py
@@ -1,11 +1,11 @@
 import tkinter as tk
-def print_something(txt):
-        print(txt)
-        tk.Label(text='confuse').pack()
+def print_something():
+    print("txt")
+    tk.Label(text='confuse').pack()
 app = tk.Tk()
 app.title('Hello world')
 e1 = tk.Entry()
 e1.pack()
-button = tk.Button(text='click me!', command=print_something(e1)).pack()
+button = tk.Button(text='click me!', command=print_something).pack()
 tk.Button(text='Looo').pack()
 app.mainloop()
